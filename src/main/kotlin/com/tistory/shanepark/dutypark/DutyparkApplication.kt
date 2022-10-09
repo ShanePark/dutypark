142ec42140f3a6ea48c40f77a225a08b97dc5e93
shane <psh40963@naver.com>
2022-10-09 15:23:08 +0900

    initial commit

---
+++ src/main/kotlin/com/tistory/shanepark/dutypark/DutyparkApplication.kt
@@ -0,0 +1,11 @@
+package com.tistory.shanepark.dutypark
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DutyparkApplication
+
+fun main(args: Array<String>) {
+    runApplication<DutyparkApplication>(*args)
+}
