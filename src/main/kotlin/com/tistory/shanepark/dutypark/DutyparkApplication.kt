7926eddee28ee54612babd232b234953d001ba6b
shane <psh40963@naver.com>
2022-11-06 22:52:48 +0900

    send slack notification async

--- src/main/kotlin/com/tistory/shanepark/dutypark/DutyparkApplication.kt
+++ src/main/kotlin/com/tistory/shanepark/dutypark/DutyparkApplication.kt
@@ -2,10 +2,22 @@ package com.tistory.shanepark.dutypark
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @SpringBootApplication
 class DutyparkApplication
 
 fun main(args: Array<String>) {
     runApplication<DutyparkApplication>(*args)
+}
+
+@Bean
+fun threadPoolTaskExecutor(): TaskExecutor {
+    val executor = ThreadPoolTaskExecutor()
+    executor.corePoolSize = 5
+    executor.maxPoolSize = 5
+    executor.initialize()
+    return executor
 }
