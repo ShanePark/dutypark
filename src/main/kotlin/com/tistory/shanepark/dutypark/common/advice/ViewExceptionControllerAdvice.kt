a0a442310cd2da17c05fa5c0060a0d7224168bc7
shane <psh40963@naver.com>
2023-05-02 23:14:04 +0900

    feat: add temporal error pages

--- src/main/kotlin/com/tistory/shanepark/dutypark/common/advice/ViewExceptionControllerAdvice.kt
+++ src/main/kotlin/com/tistory/shanepark/dutypark/common/advice/ViewExceptionControllerAdvice.kt
@@ -10,9 +10,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @Order(Ordered.LOWEST_PRECEDENCE)
 class ViewExceptionControllerAdvice {
 
+    val log: org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
+
     @ExceptionHandler
     fun notAuthorizedHandler(e: DutyparkAuthException): String {
         return "redirect:/login"
+    }
+
+    @ExceptionHandler
+    fun noSuchElementHandler(e: NoSuchElementException): String {
+        log.info("no such element: ${e.message}")
+        return "error/404"
     }
 
 }
