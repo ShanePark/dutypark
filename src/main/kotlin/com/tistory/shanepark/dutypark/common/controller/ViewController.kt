37ec7a7951757db89da539bd4cdeed4221053f35
shane <psh40963@naver.com>
2023-05-21 22:20:01 +0900

    style: layout UI enhance
    TODO: Mobile UI

---
+++ src/main/kotlin/com/tistory/shanepark/dutypark/common/controller/ViewController.kt
@@ -0,0 +1,12 @@
+package com.tistory.shanepark.dutypark.common.controller
+
+import org.springframework.ui.Model
+
+open class ViewController {
+
+    protected fun layout(model: Model, menu: String): String {
+        model.addAttribute("content", "${menu}");
+        return "layout/layout"
+    }
+
+}
--- src/main/kotlin/com/tistory/shanepark/dutypark/duty/controller/DutyViewController.kt
+++ src/main/kotlin/com/tistory/shanepark/dutypark/duty/controller/DutyViewController.kt
@@ -1,11 +1,8 @@
 package com.tistory.shanepark.dutypark.duty.controller
 
-import com.tistory.shanepark.dutypark.common.exceptions.DutyparkAuthException
-import com.tistory.shanepark.dutypark.duty.service.DutyService
-import com.tistory.shanepark.dutypark.member.domain.annotation.Login
+import com.tistory.shanepark.dutypark.common.controller.ViewController
 import com.tistory.shanepark.dutypark.member.domain.dto.MemberDto
 import com.tistory.shanepark.dutypark.member.service.MemberService
-import com.tistory.shanepark.dutypark.security.domain.dto.LoginMember
 import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.Logger
 import org.springframework.stereotype.Controller
@@ -18,9 +15,7 @@ import java.time.LocalDate
 @Controller
 class DutyViewController(
     val memberService: MemberService,
-    val dutyService: DutyService,
-) {
-
+) : ViewController() {
     val log: Logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
 
     @GetMapping("/duty/{name}")
@@ -34,31 +29,7 @@ class DutyViewController(
         model.addAttribute("member", MemberDto(member))
         model.addAttribute("year", year?.let { it } ?: LocalDate.now().year)
         model.addAttribute("month", month?.let { it } ?: LocalDate.now().monthValue)
-        return "duty/duty"
-    }
-
-    @GetMapping("/duty/edit")
-    fun editDuty(
-        @RequestParam memberId: Long,
-        @RequestParam year: Int,
-        @RequestParam month: Int,
-        model: Model,
-        @Login loginMember: LoginMember
-    ): String {
-        val member = memberService.findById(memberId)
-
-        if (!dutyService.canEdit(loginMember, member)) {
-            val message =
-                "login member and request duty member does not match: login:$loginMember.id, dutyMemberId:${member.id}"
-            log.warn(message)
-            throw DutyparkAuthException(message)
-        }
-
-        model.addAttribute("member", MemberDto(member))
-        model.addAttribute("year", year)
-        model.addAttribute("month", month)
-
-        return "duty/duty-edit"
+        return layout(model, "duty/duty")
     }
 
 }
