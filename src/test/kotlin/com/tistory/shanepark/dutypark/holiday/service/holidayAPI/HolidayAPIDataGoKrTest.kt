8ba5c9d3e57795403f4a5ad71a25149ab301e22c
shane <psh40963@naver.com>
2023-05-29 10:35:50 +0900

    test: Mock api client

--- src/test/kotlin/com/tistory/shanepark/dutypark/holiday/service/holidayAPI/HolidayAPIDataGoKrTest.kt
+++ src/test/kotlin/com/tistory/shanepark/dutypark/holiday/service/holidayAPI/HolidayAPIDataGoKrTest.kt
@@ -1,14 +1,14 @@
 package com.tistory.shanepark.dutypark.holiday.service.holidayAPI
 
 import com.tistory.shanepark.dutypark.common.datagokr.DataGoKrApi
-import com.tistory.shanepark.dutypark.common.datagokr.DataGoKrConfig
 import com.tistory.shanepark.dutypark.holiday.service.HolidayServiceTest
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
 import java.util.stream.IntStream
 
 @ExtendWith(MockitoExtension::class)
@@ -16,10 +16,12 @@ class HolidayAPIDataGoKrTest {
 
     private val holiday2023 = HolidayServiceTest.holiday2023Dto()
 
+    private val apiResponse =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><response><header><resultCode>00</resultCode><resultMsg>NORMAL SERVICE.</resultMsg></header><body><items><item><dateKind>01</dateKind><dateName>1월1일</dateName><isHoliday>Y</isHoliday><locdate>20230101</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230121</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230122</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230123</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>대체공휴일</dateName><isHoliday>Y</isHoliday><locdate>20230124</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>삼일절</dateName><isHoliday>Y</isHoliday><locdate>20230301</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>어린이날</dateName><isHoliday>Y</isHoliday><locdate>20230505</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>부처님오신날</dateName><isHoliday>Y</isHoliday><locdate>20230527</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>대체공휴일</dateName><isHoliday>Y</isHoliday><locdate>20230529</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>현충일</dateName><isHoliday>Y</isHoliday><locdate>20230606</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>제헌절</dateName><isHoliday>N</isHoliday><locdate>20230717</locdate><remarks>국경일이지만 공휴일 아님</remarks><seq>1</seq></item><item><dateKind>01</dateKind><dateName>광복절</dateName><isHoliday>Y</isHoliday><locdate>20230815</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230928</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230929</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230930</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>개천절</dateName><isHoliday>Y</isHoliday><locdate>20231003</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>한글날</dateName><isHoliday>Y</isHoliday><locdate>20231009</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>기독탄신일</dateName><isHoliday>Y</isHoliday><locdate>20231225</locdate><seq>1</seq></item></items><numOfRows>100</numOfRows><pageNo>1</pageNo><totalCount>18</totalCount></body></response>"
+
     @Test
-    @Disabled("API Key is required")
-    fun `getHolidayInfoTest`() {
-        val dataGoKrApi = DataGoKrConfig().dataGoApi()
+    fun `getHolidayInfoTest`(@Mock dataGoKrApi: DataGoKrApi) {
+        `when`(dataGoKrApi.getHolidays(any(), any())).thenReturn(apiResponse)
 
         val holidayAPIDataGoKr = HolidayAPIDataGoKr(dataGoKrApi)
         HolidayAPIDataGoKr::class.java.getDeclaredField("serviceKey").apply {
@@ -37,10 +39,8 @@ class HolidayAPIDataGoKrTest {
 
     @Test
     fun `parseTest`(@Mock dataGoKrApi: DataGoKrApi) {
-        val response =
-            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><response><header><resultCode>00</resultCode><resultMsg>NORMAL SERVICE.</resultMsg></header><body><items><item><dateKind>01</dateKind><dateName>1월1일</dateName><isHoliday>Y</isHoliday><locdate>20230101</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230121</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230122</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>설날</dateName><isHoliday>Y</isHoliday><locdate>20230123</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>대체공휴일</dateName><isHoliday>Y</isHoliday><locdate>20230124</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>삼일절</dateName><isHoliday>Y</isHoliday><locdate>20230301</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>어린이날</dateName><isHoliday>Y</isHoliday><locdate>20230505</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>부처님오신날</dateName><isHoliday>Y</isHoliday><locdate>20230527</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>대체공휴일</dateName><isHoliday>Y</isHoliday><locdate>20230529</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>현충일</dateName><isHoliday>Y</isHoliday><locdate>20230606</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>제헌절</dateName><isHoliday>N</isHoliday><locdate>20230717</locdate><remarks>국경일이지만 공휴일 아님</remarks><seq>1</seq></item><item><dateKind>01</dateKind><dateName>광복절</dateName><isHoliday>Y</isHoliday><locdate>20230815</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230928</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230929</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>추석</dateName><isHoliday>Y</isHoliday><locdate>20230930</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>개천절</dateName><isHoliday>Y</isHoliday><locdate>20231003</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>한글날</dateName><isHoliday>Y</isHoliday><locdate>20231009</locdate><seq>1</seq></item><item><dateKind>01</dateKind><dateName>기독탄신일</dateName><isHoliday>Y</isHoliday><locdate>20231225</locdate><seq>1</seq></item></items><numOfRows>100</numOfRows><pageNo>1</pageNo><totalCount>18</totalCount></body></response>"
         val holidayAPIDataGoKr = HolidayAPIDataGoKr(dataGoKrApi)
-        val result = holidayAPIDataGoKr.parse(response)
+        val result = holidayAPIDataGoKr.parse(apiResponse)
         Assertions.assertThat(result).hasSize(holiday2023.size)
         IntStream.range(0, holiday2023.size).forEach { i ->
             // Holiday name can be different depending on the API.
