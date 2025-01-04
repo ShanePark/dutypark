be5231f902b722b66a946f7c0f09c4f1082b2006
shane <psh40963@naver.com>
2025-01-04 23:05:02 +0900

    fix: Spring version back to 3.3.5 due to Hibernate 6.6.xbug

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.3.5"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
