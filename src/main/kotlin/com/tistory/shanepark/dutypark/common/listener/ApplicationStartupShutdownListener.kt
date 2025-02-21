e1e3092bf973e906823f310aeaf77d5b4b291ec9
shane <psh40963@naver.com>
2025-02-21 21:53:19 +0900

    fix: memory leak - should not disturb test

--- src/main/kotlin/com/tistory/shanepark/dutypark/common/listener/ApplicationStartupShutdownListener.kt
+++ src/main/kotlin/com/tistory/shanepark/dutypark/common/listener/ApplicationStartupShutdownListener.kt
@@ -5,12 +5,14 @@ import net.gpedro.integrations.slack.SlackMessage
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.ContextClosedEvent
 import org.springframework.context.event.EventListener
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import kotlin.system.exitProcess
 
 @Component
 class ApplicationStartupShutdownListener(
-    private val slackNotifier: SlackNotifier
+    private val slackNotifier: SlackNotifier,
+    private val environment: Environment
 ) {
 
     @EventListener(ApplicationReadyEvent::class)
@@ -23,7 +25,9 @@ class ApplicationStartupShutdownListener(
     fun onApplicationShutdown(event: ContextClosedEvent) {
         val slackMessage = makeSlackMessage(text = "Application is shutting down")
         slackNotifier.call(slackMessage)
-        exitProcess(0)
+        if (!environment.activeProfiles.contains("test")) {
+            exitProcess(0)
+        }
     }
 
     fun makeSlackMessage(text: String): SlackMessage {
