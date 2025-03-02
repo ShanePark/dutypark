113d7f92f37dcaf98859c97900aa160eeaa83f1c
shane <psh40963@naver.com>
2025-03-02 21:09:10 +0900

    test: ScheduleTimeExtractionService

--- src/test/kotlin/com/tistory/shanepark/dutypark/schedule/timeextract/service/ScheduleTimeExtractionServiceTest.kt
+++ src/test/kotlin/com/tistory/shanepark/dutypark/schedule/timeextract/service/ScheduleTimeExtractionServiceTest.kt
@@ -16,6 +16,7 @@ class ScheduleTimeExtractionServiceTest {
     @Test
     @Disabled("External API test")
     fun extractScheduleTime() {
+        // Given
         val apiKey = "PUT_KEY_HERE_for_external_integration_test"
         val openapi = OpenAiApi
             .builder()
@@ -41,17 +42,52 @@ class ScheduleTimeExtractionServiceTest {
             objectMapper = jsr310ObjectMapper()
         )
 
-        val request = ScheduleTimeExtractionRequest(
-            date = LocalDate.of(2025, 2, 28),
-            content = "친구들과 밤 11시에 만나기"
+
+        // Then
+        val response = service.extractScheduleTime(
+            ScheduleTimeExtractionRequest(
+                date = LocalDate.of(2025, 2, 28),
+                content = "친구들과 밤 11시에 만나기"
+            )
         )
-        val response = service.extractScheduleTime(request)
 
         Assertions.assertThat(response.result).isTrue()
         Assertions.assertThat(response.hasTime).isTrue()
         Assertions.assertThat(response.dateTime).isEqualTo("2025-02-28T23:00:00")
         Assertions.assertThat(response.content).doesNotContain("11시")
-    }
 
+        val response2 = service.extractScheduleTime(
+            ScheduleTimeExtractionRequest(
+                date = LocalDate.of(2025, 2, 28),
+                content = "다섯시 저녁약속"
+            )
+        )
+        Assertions.assertThat(response2.result).isTrue()
+        Assertions.assertThat(response2.hasTime).isTrue()
+        Assertions.assertThat(response2.dateTime).isEqualTo("2025-02-28T17:00:00")
+        Assertions.assertThat(response2.content).doesNotContain("다섯시")
+
+        val response3 = service.extractScheduleTime(
+            ScheduleTimeExtractionRequest(
+                date = LocalDate.of(2025, 2, 28),
+                content = "11:30세탁기설치"
+            )
+        )
+        Assertions.assertThat(response3.result).isTrue()
+        Assertions.assertThat(response3.hasTime).isTrue()
+        Assertions.assertThat(response3.dateTime).isEqualTo("2025-02-28T11:30:00")
+        Assertions.assertThat(response3.content).doesNotContain("11:30")
+
+        val response4 = service.extractScheduleTime(
+            ScheduleTimeExtractionRequest(
+                date = LocalDate.of(2025, 2, 28),
+                content = "오사카 여행"
+            )
+        )
+        Assertions.assertThat(response4.result).isTrue()
+        Assertions.assertThat(response4.hasTime).isFalse()
+        Assertions.assertThat(response4.dateTime).isNull()
+        Assertions.assertThat(response4.content).isEqualTo("오사카 여행")
+    }
 
 }
